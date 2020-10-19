99e09a138d9a443fdcf251d36bc745138444cd77
ayaanqui <moahammedayaan.dev@gmail.com>
2020-10-19 12:50:25 -0500

    Gets random Wikipedia article with title and summary

--- walk/app.py
+++ walk/app.py
@@ -6,8 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    page = requests.get('https://en.wikipedia.org/wiki/Special:Random')
+    data = BeautifulSoup(page.content, 'html.parser')
+    title = data.find('h1', {"id": "firstHeading"}).text
+    content = data.find('p').text
+
+    return render_template('index.html', context={"content":  content, "title": title})
